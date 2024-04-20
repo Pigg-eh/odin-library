6aa53b7be7166c2242f24f169fb95a9dd28a9e93
Ed Hunter <ep.hunter23@gmail.com>
2024-04-20 11:27:11 +0800

    feat: myLibrary iterates only through nonfunctions

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
 
+
 function Book (title, author, pages, status) {
     this.title = title
     this.author = author
@@ -10,35 +11,59 @@ function Book (title, author, pages, status) {
     }
   }
 
-  createTable()
-  function createTable () {
-    let headingNames = ["Title", "Author", "Pages", "Status"]
-    let table = document.querySelector('table.library');
-    let rows = document.createElement("tr")
-      headingNames.forEach((arr) => {
+
+
+  insertBook ()
+  function insertBook () {
+    
+   
+      let rows = document.createElement("tr")
+      let table = document.querySelector('table.library');
+      table.appendChild(rows)
+
+    
+    let info = ["Title", "Author", "Pages", "Status"]
+      info.forEach((arr) => {
+        
         let tableHeader = document.createElement('th');
         tableHeader.textContent = arr;
         rows.appendChild(tableHeader);
-      });
-
-      table.appendChild(rows)
-
-      // table.appendChild(tableHeader)
-
+      }); 
   }
+
+  //attempt at populating table 
+  
+  function populateTable (myLibrary, length) {
+
+    let rows = document.createElement("tr")
+    let table = document.querySelector('table.library');
+
+     
+
+    for (i=0; i < length; i++){
+      table.appendChild(rows)
+      
+        Object.values(myLibrary[i]).forEach((key) => {
+          if(typeof(key) != "function"){
+
+            console.log(typeof(key))
+            let tableRow = document.createElement('td');
+            tableRow.textContent = key;
+            rows.append(tableRow);
+         }
+        });
+        
+      } 
+    }
+  
+
+
 
   function addBookToLibrary (title, author, pages, status) {
     myLibrary.push(new Book(title, author, pages, status))
   }
 
-  function insertBook () { //adds books to library dom maniputlation 
-    
 
-    for (book in myLibrary){
-      
-    }
-
-  }
   
 
   //TEST BOOKS//
@@ -49,6 +74,8 @@ function Book (title, author, pages, status) {
   addBookToLibrary ("The Wise Mans Fear", "Patrick Rothfuss", 700, "curently reading")
 
   addBookToLibrary ("The Guest List", "Gillian Flynn", 320, "haven't read")
+
+  populateTable (myLibrary, myLibrary.length)
 
 
 
